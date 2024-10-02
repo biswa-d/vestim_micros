f5fbfaef1fff80c8b51c6e799709db7a9a5bbd6f
biswa-d <dsign.biswa@gmail.com>
2024-10-02 15:06:47 -0400

    cuda:1

--- vestim/gateway/src/testing_manager_qt_test_1.py
+++ vestim/gateway/src/testing_manager_qt_test_1.py
@@ -26,7 +26,7 @@ class VEstimTestingManager:
         self.training_setup_manager = VEstimTrainingSetupManager()
         self.testing_service = VEstimTestingService()
         self.test_data_service = VEstimTestDataService()
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
         self.max_workers = 4  # Number of concurrent threads
         self.queue = None  # Initialize the queue attribute
         self.stop_flag = False  # Initialize the stop flag attribute
