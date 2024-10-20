9f433d0ee6f2c19f7c723f90f5f1b298d2ac9a07
biswa-d <dsign.biswa@gmail.com>
2024-10-20 14:29:11 -0400

    updated to 6000 exploration

--- vestim/gateway/src/training_task_manager_qt_test.py
+++ vestim/gateway/src/training_task_manager_qt_test.py
@@ -219,9 +219,9 @@ class TrainingTaskManager:
             self.pso = PSOWeightsAndLR(n_particles=10, model=model, lr_range=[1e-5, 2e-4])
             pso = self.pso
             # Assuming lr_update_interval is set for different phases
-            exploration_lr_update_interval = 1000  # Update every 70 epochs during exploration
-            exploitation_lr_update_interval = 800  # Update every 30 epochs during exploitation
-            exploration_epochs = 4000  # Set how long you want the exploration phase to last (in validation epochs)
+            exploration_lr_update_interval = 1500  # Update every 70 epochs during exploration
+            exploitation_lr_update_interval = 1000  # Update every 30 epochs during exploitation
+            exploration_epochs = 6000  # Set how long you want the exploration phase to last (in validation epochs)
            
 
             # Log the training progress for each epoch
