5e0e9376e3d5dd0b5029db98a0ec620c6d87fcc7
biswa-d <dsign.biswa@gmail.com>
2025-02-14 16:56:42 -0500

    data_processing updated the train test folder

--- vestim/services/data_processor/src/data_processor_qt_tesla.py
+++ vestim/services/data_processor/src/data_processor_qt_tesla.py
@@ -32,10 +32,10 @@ class DataProcessorTesla:
         self.switch_log_file(job_log_file)
 
         # Create directories for raw and processed data
-        train_raw_folder = os.path.join(job_folder, 'train', 'raw_data')
-        train_processed_folder = os.path.join(job_folder, 'train', 'processed_data')
-        test_raw_folder = os.path.join(job_folder, 'test', 'raw_data')
-        test_processed_folder = os.path.join(job_folder, 'test', 'processed_data')
+        train_raw_folder = os.path.join(job_folder, 'train_data', 'raw_data')
+        train_processed_folder = os.path.join(job_folder, 'train_data', 'processed_data')
+        test_raw_folder = os.path.join(job_folder, 'test_data', 'raw_data')
+        test_processed_folder = os.path.join(job_folder, 'test_data', 'processed_data')
 
         os.makedirs(train_raw_folder, exist_ok=True)
         os.makedirs(train_processed_folder, exist_ok=True)
