b0d903a0bf9cdb617042b497cb2cd8d35f32fec6
biswa-d <dsign.biswa@gmail.com>
2025-07-20 11:01:18 -0400

    updated

--- vestim/gui/src/optuna_optimization_gui_qt.py
+++ vestim/gui/src/optuna_optimization_gui_qt.py
@@ -261,7 +261,9 @@ class OptunaOptimizationThread(QThread):
             model_service = model_service_class()
 
             device = torch.device(params.get('DEVICE_SELECTION', 'cuda:0') if torch.cuda.is_available() else 'cpu')
-            model = model_service.create_model_for_optuna(params, device=device)
+            if params['MODEL_TYPE'] == 'FNN' and 'INPUT_SIZE' not in params:
+                params['INPUT_SIZE'] = len(self.params['FEATURE_COLUMNS'])
+            model = model_service.create_model(params, device=device)
             
             training_task = {
                 'task_id': f"optuna_trial_{trial.number}",
